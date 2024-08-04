6c47140c0b8d33724c654131462200d9fdced0ab
jedi <coding.jedi.knight@gmail.com>
2024-08-04 12:38:14 +0300

    feat: add system.d service

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # rpi-youtube-socks-proxy
 
 ## Install
+```bash
 curl -fsSL https://raw.githubusercontent.com/CodingJediKnight/rpi-youtube-socks-proxy/main/install.sh | bash
+sudo systemctl daemon-reload
+sudo systemctl start byedpi.service
+sudo systemctl status byedpi.service
+sudo systemctl enable byedpi.service
+```
--- install.sh
+++ install.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 
+_have() { type "$1" &>/dev/null; }
+! _have jq && echo "requires jq (apt install jq)" && exit 1
+
+# byedpi install
+
 user="hufrea"
 repo="byedpi"
 arch=$(uname -m)
 latest=$(curl -sSL "https://api.github.com/repos/$user/$repo/releases/latest" | jq -r .tag_name)
 version=$(echo $latest | cut -d. -f2)
 version+=$(echo $latest | cut -d. -f3)
-rel="https://github.com/hufrea/byedpi/releases/download/$latest/byedpi-$version-$arch.tar.gz"
+rel="https://github.com/$user/$repo/releases/download/$latest/byedpi-$version-$arch.tar.gz"
 folder="$HOME/.local/bin"
 mkdir -p "$folder"
-echo "Downloading $rel to $folder"
-curl -L "$rel" -o "$folder/byedpi.tar.gz"
-tar -xzvf "$folder/byedpi.tar.gz" && rm -rf "$folder/byedpi.tar.gz"
-mv "$folder/ciadpi-$arch" "$folder/ciadpi"
+
+echo "Downloading $rel to $folder" && \
+curl -SL "$rel" -o "$folder/byedpi.tar.gz" && \
+tar -xzvf "$folder/byedpi.tar.gz" -C "$folder" && \
+rm -f "$folder/byedpi.tar.gz" && \
+mv "$folder/ciadpi-$arch" "$folder/byedpi"
+
+# service install
+
+sudo touch /etc/systemd/system/byedpi.service
+sudo bash -c "cat << EOF > /etc/systemd/system/byedpi.service
+[Unit]
+Description=ByeDPI Youtube fix
+ConditionFileIsExecutable=$folder/byedpi
+
+After=syslog.target network.target
+
+[Service]
+StartLimitInterval=5
+StartLimitBurst=10
+ExecStart=$folder/byedpi --hosts :\"youtube.com youtu.be googlevideo.com ytimg.com\" -p 7755 --tlsrec 3 --disorder 3
+
+Restart=always
+RestartSec=120
+
+[Install]
+WantedBy=multi-user.target
+EOF"
